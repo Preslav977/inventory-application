b9c02e0f4c73640011cc0c0ca460874ed7ace37b
Preslav977 <p.cvetanow@gmail.com>
2024-03-20 08:48:43 +0200

    Created the author schema.

---
+++ models/author.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const AuthorSchema = new Schema({
+  name: { type: String, minLength: 5, maxLength: 30, required: true },
+  featured: { type: String, minLength: 5, maxLength: 30, required: true },
+  date_of_birth: { type: Date },
+  single: { type: Boolean },
+  socials: { type: Boolean },
+});
+
+// if author doesn't have name return empty string
+
+AuthorSchema.virtual("name").get(function () {
+  let authorName = "";
+
+  if (this.name) {
+    authorName = `${this.name}`;
+  }
+
+  return authorName;
+});
+
+AuthorSchema.virtual("url").get(function () {
+  return `/store/author/${this._id}`;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
