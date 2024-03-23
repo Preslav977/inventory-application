c42b2cb8857911100cc4ee9e17629c2dffb04062
Preslav977 <p.cvetanow@gmail.com>
2024-03-23 11:27:00 +0200

    Created the routes for genres

--- routes/store.js
+++ routes/store.js
@@ -47,3 +47,19 @@ router.get("/author/:id", author_controller.author_detail);
 router.get("/authors", author_controller.author_list);
 
 /// ////////////////////////////////////////////////////////////////////
+
+router.get("/genre/create", genre_controller.genre_create_get);
+
+router.post("/genre/create", genre_controller.genre_create_post);
+
+router.get("/genre/:id/delete", genre_controller.genre_delete_get);
+
+router.post("/genre/:id/delete", genre_controller.genre_delete_post);
+
+router.get("/genre/:id/update", genre_controller.genre_update_get);
+
+router.post("/genre/:id/update", genre_controller.genre_update_post);
+
+router.get("/genre/:id", genre_controller.genre_detail);
+
+router.get("/genres", genre_controller.genre_list);
