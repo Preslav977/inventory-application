157f705365cb74fa91e5a74a90924fb9bd8b8eb8
Preslav977 <p.cvetanow@gmail.com>
2024-03-23 11:23:00 +0200

    Implemented the routers for author

--- routes/store.js
+++ routes/store.js
@@ -27,3 +27,23 @@ router.post("/song/:id/update", song_controller.song_update_post);
 router.get("/book/:id", song_controller.song_detail);
 
 router.get("/songs", song_controller.song_list);
+
+/// /////////////////////////////////////////////////////////////////
+
+router.get("/author/create", author_controller.author_create_get);
+
+router.post("/author/create", author_controller.author_create_post);
+
+router.get("/author/:id/delete", author_controller.author_delete_get);
+
+router.post("/author/:id/delete", author_controller.author_delete_post);
+
+router.get("/author/:id/update", author_controller.author_update_get);
+
+router.post("/author/:id/update", author_controller.author_update_post);
+
+router.get("/author/:id", author_controller.author_detail);
+
+router.get("/authors", author_controller.author_list);
+
+/// ////////////////////////////////////////////////////////////////////
