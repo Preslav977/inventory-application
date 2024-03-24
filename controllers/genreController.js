fe66940dd0629ff5f49957e8c050b8ace5c47590
Preslav977 <p.cvetanow@gmail.com>
2024-03-24 11:14:37 +0200

    Implemented genre detail

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Genre = require("../models/genre");
+const Song = require("../models/song");
 
 exports.genre_list = asyncHandler(async (req, res, next) => {
   const allGenres = await Genre.find().exec();
@@ -11,7 +12,22 @@ exports.genre_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Genre detail:  ${req.params.id}`);
+  const [genre, songsInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Song.find({ genre: req.params.id }, "title").exec(),
+  ]);
+
+  if (genre === null) {
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_detail", {
+    title: "Genre Detail",
+    genre,
+    genre_songs: songsInGenre,
+  });
 });
 
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
