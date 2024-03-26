86e8f3d52e6b077a6da95e0596457b8c29ef6549
Preslav977 <p.cvetanow@gmail.com>
2024-03-26 11:12:34 +0200

    Creating new author is implemented.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Author = require("../models/author");
 const Song = require("../models/song");
 
@@ -30,13 +31,50 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.author_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
-});
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create New Author" });
+};
 
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-});
+exports.author_create_post = [
+  body("name")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 30 })
+    .withMessage("Name must be specified"),
+
+  body("featured")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 30 })
+    .withMessage("Name of the vocalist must be specified"),
+
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const author = new Author({
+      name: req.body.name,
+      featured: req.body.featured,
+      date_of_birth: req.body.date_of_birth,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Create New Author",
+        author,
+        errors: errors.array(),
+      });
+    } else {
+      await author.save();
+
+      res.redirect(author.url);
+    }
+  }),
+];
 
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete GET");
